42c31e741038700f2573f4d416754169c89d1e62
Jensen Koch <jtkoch12@gmail.com>
2020-05-03 20:01:53 -0500

    added seeds

--- migrations/20200503182028_initial.js
+++ migrations/20200503182028_initial.js
@@ -51,7 +51,7 @@ exports.up = async function(knex) {
 
 exports.down = async function(knex) {
   await knex.schema.dropTableIfExists("project_resources")
-  await knex.schema.dropTableIfExists("project_tasks")
+  await knex.schema.dropTableIfExists("project_task")
   await knex.schema.dropTableIfExists("resources")
   await knex.schema.dropTableIfExists("tasks")
   await knex.schema.dropTableIfExists("projects")
